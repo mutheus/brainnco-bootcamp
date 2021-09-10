60180b2e51ad41cf4b07e7486023cd4f3b8f8f1e
Matheus Almeida <matheusalmeida016@gmail.com>
2021-09-10 08:41:08 -0400

    Add prettier config

--- challenge-04/challenge-03-styled/client/src/app.js
+++ challenge-04/challenge-03-styled/client/src/app.js
@@ -1,89 +1,89 @@
-import { useState, useEffect } from 'react';
-import { get, post, del } from './services/http';
-import { Form } from './components/form';
-import { Table } from './components/table';
-import { Alert } from './components/alert';
-import * as S from './styles';
+import { useState, useEffect } from "react";
+import { get, post, del } from "./services/http";
+import { Form } from "./components/form";
+import { Table } from "./components/table";
+import { Alert } from "./components/alert";
+import * as S from "./styles";
 
-import GlobalStyle from './global-style';
+import GlobalStyle from "./global-style";
 
-const url = 'http://localhost:3333/cars';
-  
-function App () {
+const url = "http://localhost:3333/cars";
+
+function App() {
   const [cars, setCars] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [isAddingCar, setIsAddingCar] = useState(false);
-  
+
   useEffect(() => {
-    async function getCars () {
+    async function getCars() {
       const result = await get(url);
-      
+
       if (result.error) {
         setMessage(result.message);
         return;
       }
-      
+
       setCars(result.reverse());
     }
-    
+
     getCars();
-  }, [])
-  
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      setMessage('');
+      setMessage("");
     }, 1000);
-    
+
     return () => clearTimeout(timer);
-  }, [message])
-  
-  async function handleSubmit (e) {
+  }, [message]);
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    
+
     const car = {
       image: e.target.elements.image.value,
       brandModel: e.target.elements.brandModel.value,
       year: e.target.elements.year.value,
       plate: e.target.elements.plate.value,
-      color: e.target.elements.color.value
-    }
-    
+      color: e.target.elements.color.value,
+    };
+
     const newCars = [car, ...cars];
-    
+
     const result = await post(url, car);
-    
+
     if (result.error) {
       setMessage(result.message);
       return;
     }
-    
+
     e.target.reset();
     setCars(newCars);
     setIsAddingCar(false);
   }
-  
-  async function handleRemoveClick (plate) {
-    const newCars = cars.filter(car => car.plate !== plate);
+
+  async function handleRemoveClick(plate) {
+    const newCars = cars.filter((car) => car.plate !== plate);
     const result = await del(url, { plate });
-    
+
     if (result.error) {
       setMessage(result.message);
       return;
     }
-    
+
     setCars(newCars);
   }
-  
-  function closeModal () {
+
+  function closeModal() {
     setIsAddingCar(false);
   }
-  
+
   return (
     <>
       <GlobalStyle />
-      
+
       {message.length > 0 && <Alert message={message} />}
-      
+
       <S.Wrapper>
         <a href="/">
           <S.Logo>mutheuswagen</S.Logo>
@@ -96,22 +96,20 @@ function App () {
             <Form handleSubmit={handleSubmit} closeModal={closeModal} />
           </div>
         )}
-        
+
         <div className="content__hero">
           <h2>Welcome, John 🤗</h2>
         </div>
-        
-        
+
         <div className="content__actions">
           <h3 className="content__title">All cars</h3>
-          
-          <button onClick={() => setIsAddingCar(true)}><span>+</span> Add car</button>
+
+          <button onClick={() => setIsAddingCar(true)}>
+            <span>+</span> Add car
+          </button>
         </div>
-        
-        <Table 
-          cars={cars} 
-          handleRemoveClick={handleRemoveClick} 
-        />
+
+        <Table cars={cars} handleRemoveClick={handleRemoveClick} />
       </S.Wrapper>
     </>
   );
